d5013411102bc5eee55fd2dcfe29734d1cc398c1
Raymond Wan <raymondwan@Raymonds-MacBook-Pro.local>
2016-11-26 14:41:13 +1100

    Asdfasf

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/webhook', function (req, res) {
 });
 
 // handler receiving messages
-app.post('/webhook', function (req, res) {
+app.post('/webhook', function (req, res)) {
 	var events = req.body.entry[0].messaging;
   for (i = 0; i < events.length; i++) {
     var event = events[i];
