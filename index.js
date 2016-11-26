bef6792f14e9e05706f8df3515f259545c650842
Mitch Ball <mitchb145@gmail.com>
2016-11-27 03:18:49 +1100

    Fixes to current code

--- index.js
+++ index.js
@@ -121,7 +121,8 @@ function sendMessage(recipientId, message) {
 function banterTheUser(recipientID, text) {
     text = text.toLowerCase() || "";
 
-    messageHelper(recipientID, {text: 'Debug: '+text});
+    // This will stop other message sending and shit it seems but will help you debug regardless
+    // messageHelper(recipientID, {text: 'Debug: '+text});
 
     switch(text){
 
@@ -161,7 +162,7 @@ function banterTheUser(recipientID, text) {
         messageHelper(recipientID, {text: 'Alright, anything else you wanted to chat about mate? Click any of the menu options if there is.'});
         break;
       case "i'm having doubts":
-        quickReply(recipientID, "Want more info", "", "",
+        quickReply(recipientID, "Want more info", "Nah", "",
         "Fair enough, It's pretty ridiculous that they make you make such a big decision so early on in your life. But you know what, I changed degrees 2 times mate, and plenty of my friends too :). Actually about 50% of students change their degree during their time at uni. Did you want more info?");
         break;
       case "want more info":
