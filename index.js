5942999fbf054933afa81bdaa8d90fafdf639fe4
Raymond Wan <raymondwan@Raymonds-MacBook-Pro.local>
2016-11-27 11:42:34 +1100

    bitchmith :>

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ function banterTheUser(recipientID, text) {
       case "study":
         messageHelper(recipientID, {text: "u just might as well get drunk"});
         break;
-      case "pressure":
+      case "pressure from others":
         // TODO make this four (missing the yourself option)
       	quickReply(recipientI, "Family", "Friends", "Teachers", "Ahh that's a shame. It's quite common as well unfortunately :/ Remember to ask people for help and advice whenever you need it by the way. Who are you feeling pressured from?");
         break;
